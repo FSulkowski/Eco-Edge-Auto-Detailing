f37f80061decb9515992e5dd38e35571f5b80d75
Francis Sulkowski <sulkowskif@yahoo.com>
2025-01-21 21:09:58 -0500

    app.py, html pages and images added

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
